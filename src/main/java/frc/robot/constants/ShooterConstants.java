cc0cae7eb9f9ee353103a5063da450a772748cb0
Sam Faw <sam.faw@outlook.com>
2023-02-09 19:58:15 -0500

    first commit of cube shooter subsystem - issue complete

---
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -0,0 +1,12 @@
+package frc.robot.constants;
+
+public class ShooterConstants {
+
+    public static final int kCubeRightMotorPort = 0;
+    public static final int kCubeLeftMotorPort = 0;
+
+    public static final int kCubeLimitSwitchPort = 0;
+
+    public final static double kCubeLoadSpeed = 0.0;
+    public final static double kCubeShooterSpeed = 0.0;
+}
---
+++ src/main/java/frc/robot/subsystems/CubeShooter.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.constants.ShooterConstants;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class CubeShooter {
+
+    private CANSparkMax leftShooterMotor = new CANSparkMax(ShooterConstants.kCubeLeftMotorPort, MotorType.kBrushless);
+    private CANSparkMax rightShooterMotor = new CANSparkMax(ShooterConstants.kCubeRightMotorPort, MotorType.kBrushless);
+
+    private DigitalInput cubeLimitSwitch = new DigitalInput(ShooterConstants.kCubeLimitSwitchPort);
+
+    public void loadCube() {
+        if (cubeLimitSwitch.get() == false) { // if circuit is closed
+            leftShooterMotor.set(0.0);
+            rightShooterMotor.set(0.0);
+        } else {
+            leftShooterMotor.set(ShooterConstants.kCubeLoadSpeed);
+            rightShooterMotor.set(ShooterConstants.kCubeLoadSpeed);
+        }
+    }
+
+    public void shootCube() {
+        leftShooterMotor.set(ShooterConstants.kCubeShooterSpeed);
+        rightShooterMotor.set(ShooterConstants.kCubeShooterSpeed);
+    }
+}
