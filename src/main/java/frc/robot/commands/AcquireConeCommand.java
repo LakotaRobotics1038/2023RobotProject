98b1501670fbec1392fdfda1fc97078cd424a372
Griffin Lamb <lamb.griffin@icloud.com>
2023-02-17 12:33:55 -0500

    started working on acquiring cone command

---
+++ src/main/java/frc/robot/commands/AcquireConeCommand.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ConeAcquisition;
+
+public class AcquireConeCommand extends CommandBase {
+    private ConeAcquisition coneAcquisition = ConeAcquisition.getInstance();
+
+    public AcquireConeCommand() {
+        this.addRequirements(coneAcquisition);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void end() {
+
+    }
+}
