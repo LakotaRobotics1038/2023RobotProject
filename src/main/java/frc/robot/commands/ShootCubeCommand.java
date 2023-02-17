c20e425351abdac4e317051ec2f81674a685e25c
Tanmay Bunga <tbunga@lr1038.local>
2023-02-17 13:12:20 -0500

    more work on shoot cube command

--- src/main/java/frc/robot/commands/ShootCubeCommand.java
+++ src/main/java/frc/robot/commands/ShootCubeCommand.java
@@ -1,11 +1,23 @@
+package frc.robot.commands;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.CubeShooter;
+import frc.robot.subsystems.CubeAcquisition;
 
 public class ShootCubeCommand extends CommandBase {
+    private CubeShooter cubeShooter = CubeShooter.getInstance();
+    private CubeAcquisition cubeAcquisition = CubeAcquisition.getInstance();
 
     public ShootCubeCommand() {
-        this.addRequirements(CubeShooter);
+        this.addRequirements(cubeShooter);
         this.addRequirements();
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void initialize() {
 
     }
 }
