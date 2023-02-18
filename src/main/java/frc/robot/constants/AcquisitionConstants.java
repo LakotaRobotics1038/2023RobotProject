0fd909eb12884cddda4abcfb7564d48dc5ff3e33
Sam Faw <sam.faw@outlook.com>
2023-02-18 09:44:01 -0500

    managing PID like shoulder, moved constants to new file

--- src/main/java/frc/robot/constants/AcquisitionConstants.java
+++ src/main/java/frc/robot/constants/AcquisitionConstants.java
@@ -4,13 +4,4 @@ public class AcquisitionConstants {
     public static final int kConeAcquisitonMotorPort = 0;
     public static final int kConeAcquisitionSensorPort = 0;
     public static final double kConeAcquisitionConstantMotorSpeed = 0.0;
-
-    public static final int kWristMotorPort = 0;
-    public static final double kWristP = 0.0;
-    public static final double kWristI = 0.0;
-    public static final double kWristD = 0.0;
-    public static final double kWristPIDMinimum = 0.0;
-    public static final double kWristPIDMaximum = 0.0;
-    public static final double kWristPIDSpeed = 0.0;
-    public static final double kWristPIDTolerance = 0.0;
 }
--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -5,15 +5,17 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
-import frc.robot.constants.AcquisitionConstants;
+import frc.robot.constants.WristConstants;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
-public class Wrist extends PIDSubsystem{
+import edu.wpi.first.math.MathUtil;
+
+public class Wrist extends PIDSubsystem {
     private static Wrist wristInstance;
 
-    private CANSparkMax wristMotor = new CANSparkMax(AcquisitionConstants.kWristMotorPort, MotorType.kBrushless);
+    private CANSparkMax wristMotor = new CANSparkMax(WristConstants.kWristMotorPort, MotorType.kBrushless);
     private AbsoluteEncoder wristEncoder = wristMotor.getAbsoluteEncoder(Type.kDutyCycle);
     private PIDController wristPIDController;
     private boolean isPIDEnabled;
@@ -26,40 +28,46 @@ public class Wrist extends PIDSubsystem{
     }
 
     private Wrist() {
-        super(new PIDController(AcquisitionConstants.kWristP, AcquisitionConstants.kWristI,
-        AcquisitionConstants.kWristD));
+        super(new PIDController(WristConstants.kWristP, WristConstants.kWristI,
+                WristConstants.kWristD));
         getController().disableContinuousInput();
     }
 
     public void setPIDTolerance() {
-        getController().setTolerance(AcquisitionConstants.kWristPIDTolerance);
+        getController().setTolerance(WristConstants.kWristPIDTolerance);
     }
 
     public void setPIDcontroller() {
-        wristPIDController.setPID(AcquisitionConstants.kWristP, AcquisitionConstants.kWristI,
-        AcquisitionConstants.kWristD);
+        wristPIDController.setPID(WristConstants.kWristP, WristConstants.kWristI,
+                WristConstants.kWristD);
     }
 
     public void enable() {
-        wristPIDController.enableContinuousInput(AcquisitionConstants.kWristPIDMinimum,
-                AcquisitionConstants.kWristPIDMinimum);
+        wristPIDController.enableContinuousInput(WristConstants.kWristPIDMinimum,
+                WristConstants.kWristPIDMinimum);
     }
 
     public void periodic() {
-        if(isPIDEnabled) {
-            wristMotor.set(wristPIDController.calculate(AcquisitionConstants.kWristPIDSpeed));
+        if (isPIDEnabled) {
+            wristMotor.set(wristPIDController.calculate(WristConstants.kWristPIDSpeed));
         }
     }
 
     public void disable() {
     }
 
+    @Override
     public void useOutput(double output, double setpoint) {
-
+        double power = MathUtil.clamp(output, -1, 1);
+        wristMotor.set(power);
     }
 
+    @Override
     public double getMeasurement() {
-        return 0.0;
+        return getWristEncoder();
     }
 
+    public double getWristEncoder() {
+        return wristEncoder.getPosition();
+    }
 }
