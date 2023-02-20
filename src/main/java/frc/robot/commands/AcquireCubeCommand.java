63bb93f8fa2dcd604ebb5f8534e1ff1786773929
Wesley Reed <reed.wesley.s@gmail.com>
2023-02-20 16:16:44 -0500

    .

--- src/main/java/frc/robot/commands/AcquireCubeCommand.java
+++ src/main/java/frc/robot/commands/AcquireCubeCommand.java
@@ -1,8 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.CubeAcquisition;
 
+import frc.robot.subsystems.CubeAcquisition;
 import frc.robot.subsystems.CubeShooter;
 
 public class AcquireCubeCommand extends CommandBase {
