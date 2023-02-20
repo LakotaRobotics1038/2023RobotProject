5b86aaa91a4f67685f8d621d40ae4815510edbdf
matunkar <122322635+matunkar@users.noreply.github.com>
2023-02-20 17:35:41 -0500

    17 shoulderposition command (#31)
    * shoulder position command, shoulder.ontarget created in shoulder subsystem
    
    * Update src/main/java/frc/robot/commands/ShoulderPositionCommand.java
    
    Co-authored-by: Wesley Reed <reed.wesley.s@gmail.com>
    
    * Update src/main/java/frc/robot/commands/ShoulderPositionCommand.java
    
    Co-authored-by: Wesley Reed <reed.wesley.s@gmail.com>
    
    * isFinished method is updated and obvious comments are removed
    
    * method isFinished fixed andobvious comments removed
    
    * whitespace
    
    ---------
    
    Co-authored-by: Maryam Tunkara <mtunkara@lr1038.local>
    Co-authored-by: Wesley Reed <reed.wesley.s@gmail.com>
    Co-authored-by: Myles Knowlin <mknowlin@lr1038.local>

---
+++ src/main/java/frc/robot/commands/ShoulderPositionCommand.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shoulder;
+
+public class ShoulderPositionCommand extends CommandBase {
+
+    private double setPoint = 0;
+    private Shoulder shoulder = Shoulder.getInstance();
+
+    public ShoulderPositionCommand(double setPoint) {
+        this.setPoint = setPoint;
+        addRequirements(shoulder);
+    }
+
+    @Override
+    public void initialize() {
+        shoulder.enable();
+        shoulder.setSetpoint(setPoint);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shoulder.onTarget();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shoulder.disable();
+    }
+}
