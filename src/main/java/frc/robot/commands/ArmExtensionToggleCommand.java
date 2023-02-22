3ed1f052344fd9aa876fd8767cfd4dc7730abfc5
Griffin Lamb <glamb@lr1038.local>
2023-02-21 19:36:20 -0500

    made the Arm Extension Toggle Command

--- src/main/java/frc/robot/commands/ArmExtensionToggleCommand.java
+++ src/main/java/frc/robot/commands/ArmExtensionToggleCommand.java
@@ -2,9 +2,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Arm.ArmExtensionStates;
 
 public class ArmExtensionToggleCommand extends CommandBase {
     private Arm arm = Arm.getInstance();
+    private ArmExtensionStates states;
 
     public ArmExtensionToggleCommand() {
         this.addRequirements(arm);
@@ -16,6 +18,13 @@ public class ArmExtensionToggleCommand extends CommandBase {
     }
 
     public void execute() {
-        arm.setArmExtensionPosition(arm.ArmExtensionStates);
+        switch (states) {
+            case In:
+                arm.setArmExtensionPosition(ArmExtensionStates.Out);
+                break;
+            case Out:
+                arm.setArmExtensionPosition(ArmExtensionStates.In);
+                break;
+        }
     }
 }
