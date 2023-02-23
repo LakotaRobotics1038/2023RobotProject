f7029134c379de96e59b762ed0aec5c3b0c4b08c
Myles Knowlin <mknowlin@lr1038.local>
2023-02-22 19:25:58 -0500

    made skeleton for #25 balance

--- src/main/java/frc/robot/commands/BalanceRobotCommand.java
+++ src/main/java/frc/robot/commands/BalanceRobotCommand.java
@@ -1,27 +1,35 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 
 public class BalanceRobotCommand extends CommandBase {
 
     private DriveTrain driveTrain = DriveTrain.getInstance();
+    private PIDController pidController = new PIDController(0, 0, 0);
 
     public BalanceRobotCommand() {
         this.addRequirements(driveTrain);
     }
 
     public boolean isFinished() {
-        boolean isPIDSet = false;
 
-        // Add code to check if PID is at a setPoint.
-        // and set booolean to true if so
-        // What is setPoint?
-
+        boolean isPIDSet = pidController.atSetpoint();
         return isPIDSet;
+
     }
 
     public void execute() {
         // Function to use a PID to balance the Robots
+        if (driveTrain.getPitch() != 10.0) {
+            if (driveTrain.getPitch() < 0) {
+                driveTrain.drive(0, 0, 0, isFinished());
+            } else {
+                driveTrain.drive(0, 0, 0, isFinished());
+
+            }
+        }
+
     }
 }
