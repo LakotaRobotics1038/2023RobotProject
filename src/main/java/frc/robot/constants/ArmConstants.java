78686115062e8be5fd2150b17239a1fbac0e8f19
Wesley Reed <reed.wesley.s@gmail.com>
2023-02-23 20:10:06 -0500

    updating constants

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.constants;
 
 public final class ArmConstants {
-    public static final int kPullInArmChannel = 0;
-    public static final int kPushOutArmChannel = 0;
+    public static final int kPullInArmChannel = 15;
+    public static final int kPushOutArmChannel = 14;
 }
