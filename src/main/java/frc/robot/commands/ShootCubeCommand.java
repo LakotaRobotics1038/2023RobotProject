010e17938ab8c4db3f9dcbe5ffaad5c4b6a95faf
Tanmay Bunga <115121000+Tanmay-zaworldo@users.noreply.github.com>
2023-02-23 19:24:45 -0500

    Shoot cube command (#33)
    * Started working on shooter command
    
    * more work on shoot cube command
    
    * more work on shoot cube command
    
    * Done with like the first 3 bullet points but I wast confused with the last 3 bullet points and tried to something which caused errors
    
    * Fixed the problems which I had last time, just requires reviewing.
    
    * made changes for shoot cube command
    
    * maybe finished changes wes wanted for cubeshooter
    
    * made changes for shoot cube command
    
    * unused var
    
    * unused funcs
    
    ---------
    
    Co-authored-by: Tanmay Bunga <tbunga@lr1038.local>
    Co-authored-by: Wesley Reed <reed.wesley.s@gmail.com>
    Co-authored-by: Griffin Lamb <lamb.griffin@icloud.com>
    Co-authored-by: Griffin Lamb <glamb@lr1038.local>

---
+++ src/main/java/frc/robot/commands/ShootCubeCommand.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.CubeShooter;
+import frc.robot.subsystems.CubeAcquisition.AcquisitionStates;
+import frc.robot.subsystems.CubeAcquisition;
+
+public class ShootCubeCommand extends CommandBase {
+    private CubeShooter cubeShooter = CubeShooter.getInstance();
+    private CubeAcquisition cubeAcquisition = CubeAcquisition.getInstance();
+
+    public ShootCubeCommand() {
+        this.addRequirements(cubeAcquisition, cubeShooter);
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void initialize() {
+        cubeAcquisition.setPosition(AcquisitionStates.Down);
+    }
+
+    public void execute() {
+        cubeShooter.shootCube();
+    }
+
+    public void end() {
+        cubeAcquisition.stopFeeder();
+        cubeAcquisition.stopAcquisition();
+    }
+}
