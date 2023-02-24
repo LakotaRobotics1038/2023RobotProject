0a642f2757fbe6da495f609d1f969db5ed7b34d1
Wesley Reed <reed.wesley.s@gmail.com>
2023-02-23 20:12:25 -0500

    operator joystick port

--- src/main/java/frc/robot/constants/IOConstants.java
+++ src/main/java/frc/robot/constants/IOConstants.java
@@ -2,5 +2,5 @@ package frc.robot.constants;
 
 public final class IOConstants {
     public static final int kDriverControllerPort = 0;
-    public static final int kOperatorControllerPort = 0;
+    public static final int kOperatorControllerPort = 1;
 }
