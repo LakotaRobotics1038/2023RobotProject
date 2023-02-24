a79f2aaddd57a7ba4811f1a3c8394f65151a0ae8
Sam Faw <sfaw@lr1038.local>
2023-02-23 19:52:30 -0500

    operator joystick mapping started, not finished

--- src/main/java/frc/robot/OperatorJoystick.java
+++ src/main/java/frc/robot/OperatorJoystick.java
@@ -1,9 +1,19 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.libraries.XboxController1038;
+import frc.robot.commands.AcquireConeCommand;
+import frc.robot.commands.AcquireCubeCommand;
+import frc.robot.commands.CubeAcquisitionPositionCommand;
+import frc.robot.commands.DisposeConeCommand;
+import frc.robot.commands.ShootCubeCommand;
 import frc.robot.constants.IOConstants;
+import frc.robot.subsystems.CubeShooter;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
-public class OperatorJoystick extends XboxController {
+public class OperatorJoystick extends XboxController1038 {
+
+    private final CubeShooter cubeShooter = CubeShooter.getInstance();
+
     private static OperatorJoystick instance;
 
     public static OperatorJoystick getIntance() {
@@ -18,6 +28,18 @@ public class OperatorJoystick extends XboxController {
         super(IOConstants.kOperatorControllerPort);
 
         // Cone Acquisition
+        super.rightBumper.whileTrue(new AcquireConeCommand());
+        super.rightTrigger.whileTrue(new DisposeConeCommand());
+
+        // Cube Acquisition
+        super.bButton.whileTrue(new AcquireCubeCommand());
+        super.yButton.whenPressed(new CubeAcquisitionPositionCommand());
+
+        // Cube Shooter
+        super.leftBumper.whileTrue(new ShootCubeCommand());
+        super.leftTrigger.whileTrue(new RunCommand(cubeShooter::loadCube, cubeShooter));
+
+        // Arm + Wrist + Shoulder
 
     }
 }
