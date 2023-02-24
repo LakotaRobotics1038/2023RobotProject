659bf7b81ae848eaf3c094d3b379edac3b0163c4
Myles Knowlin <mknowlin@lr1038.local>
2023-02-24 18:44:35 -0500

    isFinished method return made simpler & pidController moved to new BalanceConstants file

--- src/main/java/frc/robot/commands/BalanceRobotCommand.java
+++ src/main/java/frc/robot/commands/BalanceRobotCommand.java
@@ -1,21 +1,19 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
+import frc.robot.constants.BalanceConstants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 
 public class BalanceRobotCommand extends CommandBase {
 
     private DriveTrain driveTrain = DriveTrain.getInstance();
-    private PIDController pidController = new PIDController(0, 0, 0);
 
     public BalanceRobotCommand() {
         this.addRequirements(driveTrain);
     }
 
     public boolean isFinished() {
-        boolean isPIDSet = pidController.atSetpoint();
-        return isPIDSet;
+        return BalanceConstants.pidController.atSetpoint();
     }
 
     public void execute() {
