154d50752dc6505fe1f3fdbd64ebbfbd765b6122
Myles Knowlin <mknowlin@lr1038.local>
2023-02-24 18:45:40 -0500

    isFinished method return made simpler & pidController moved to new BalanceConstants file

---
+++ src/main/java/frc/robot/constants/BalanceConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+import edu.wpi.first.math.controller.PIDController;
+
+public class BalanceConstants {
+
+    public static PIDController pidController = new PIDController(0, 0, 0);
+
+}
