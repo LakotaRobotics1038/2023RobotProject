e05813a1a93e5e119ef87769ca2907166846eb2d
Wesley Reed <reed.wesley.s@gmail.com>
2023-02-27 18:47:41 -0500

    replace playing with fusion sensor with rev robotics sensor

--- src/main/java/frc/robot/constants/ConeAcquisitionConstants.java
+++ src/main/java/frc/robot/constants/ConeAcquisitionConstants.java
@@ -2,6 +2,5 @@ package frc.robot.constants;
 
 public final class ConeAcquisitionConstants {
     public static final int kAcquisitionMotorPort = 12;
-    public static final int kAcquisitionSensorPort = 0;
     public static final double kConstantMotorSpeed = 0.5;
 }
