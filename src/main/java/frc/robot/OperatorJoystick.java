36535aba0718de2138f6c80aa60ecef4a7282b8e
Myles Knowlin <mknowlin@lr1038.local>
2023-02-27 19:35:34 -0500

    initial draft of Operator Joystick

--- src/main/java/frc/robot/OperatorJoystick.java
+++ src/main/java/frc/robot/OperatorJoystick.java
@@ -5,9 +5,12 @@ import frc.robot.subsystems.CubeShooter.CubeShooterSetpoints;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.AcquireConeCommand;
 import frc.robot.commands.AcquireCubeCommand;
+import frc.robot.commands.ArmExtensionCommand;
 import frc.robot.commands.CubeAcquisitionPositionCommand;
 import frc.robot.commands.DisposeConeCommand;
+import frc.robot.commands.ManualShootCubeCommand;
 import frc.robot.commands.ShootCubeCommand;
+import frc.robot.commands.ShoulderPositionCommand;
 import frc.robot.constants.IOConstants;
 
 public class OperatorJoystick extends XboxController1038 {
@@ -26,19 +29,36 @@ public class OperatorJoystick extends XboxController1038 {
         super(IOConstants.kOperatorControllerPort);
 
         // Cone Acquisition
-        super.rightBumper.whileTrue(new AcquireConeCommand());
-        super.rightTrigger.whileTrue(new DisposeConeCommand());
+        super.leftTrigger.whileTrue(new AcquireConeCommand());
+        super.leftBumper.whileTrue(new DisposeConeCommand());
 
         // Cube Acquisition
-        super.bButton.whileTrue(new AcquireCubeCommand());
-        super.yButton.onTrue(new CubeAcquisitionPositionCommand());
+        super.rightTrigger.whileTrue(new AcquireCubeCommand());
+        // TODO replace CubeAcquisitionPositionCommand() with DisposeCubeCommand()
+        super.rightBumper.onTrue(new CubeAcquisitionPositionCommand());
 
         // Cube Shooter
-        ShootCubeCommand shootCubeCommand = new ShootCubeCommand(CubeShooterSetpoints.high);
-        super.leftBumper.whileTrue(shootCubeCommand);
-        super.leftTrigger.onTrue(new InstantCommand(() -> shootCubeCommand.overrideFeed()));
+        ShootCubeCommand highShootCubeCommand = new ShootCubeCommand(CubeShooterSetpoints.high);
+        super.xButton.whileTrue(highShootCubeCommand);
+
+        ShootCubeCommand lowShootCubeCommand = new ShootCubeCommand(CubeShooterSetpoints.low);
+        super.aButton.whileTrue(lowShootCubeCommand);
 
         // Arm + Wrist + Shoulder
+        // b toggle arms in and arms out
+        super.bButton.onTrue(new ArmExtensionCommand());
 
+        // y manual revving shooter wheels
+        super.yButton.whileTrue(new ManualShootCubeCommand());
+
+        /*
+         * TODO we have options for controlling the robot:
+         * 1. Use joystick with not PID to send direct power to the shoulder
+         * 2. Use joystick with PID to set a setpoint
+         * 3. Have buttons with predefined setpoints
+         */
+
+        // left stick move shoulder
+        // right stick move wrist
     }
 }
