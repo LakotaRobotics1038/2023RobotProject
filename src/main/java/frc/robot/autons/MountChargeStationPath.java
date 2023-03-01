d74de81e3b15eb4af28a38b7e377bd4b5269ab0e
Wesley Reed <reed.wesley.s@gmail.com>
2023-03-01 09:20:14 -0500

    setX after mounting charge station

--- src/main/java/frc/robot/autons/MountChargeStationPath.java
+++ src/main/java/frc/robot/autons/MountChargeStationPath.java
@@ -2,11 +2,14 @@ package frc.robot.autons;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
+
 public class MountChargeStationPath extends Auton {
     public MountChargeStationPath() {
         PathPlannerTrajectory trajectory = Trajectories.MountChargeStationPath();
         super.addCommands(
-                this.driveTrain.getTrajectoryCommand(trajectory));
+                this.driveTrain.getTrajectoryCommand(trajectory),
+                new RunCommand(() -> driveTrain.setX(), driveTrain));
         this.setInitialPose(trajectory);
     }
 }
