819b9f7aea381529f7c34583ec412a69f102eea6
Nathan Burden <nburden355@gmail.com>
2023-03-02 15:32:52 -0500

    Adjusted auton paths; modified cube shooter

--- src/main/java/frc/robot/autons/Trajectories.java
+++ src/main/java/frc/robot/autons/Trajectories.java
@@ -6,33 +6,33 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import frc.robot.constants.AutoConstants;
 
 public class Trajectories {
-    public static PathPlannerTrajectory LeaveCommunityPathCenter() {
-        return PathPlanner.loadPath("Leave Community Path (Center)",
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    }
+    // public static PathPlannerTrajectory LeaveCommunityPathCenter() {
+    // return PathPlanner.loadPath("Leave Community Path (Center)",
+    // AutoConstants.kMaxSpeedMetersPerSecond,
+    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    // }
 
-    public static PathPlannerTrajectory LeaveCommunityPathScoringTable() {
-        return PathPlanner.loadPath("Leave Community Path (Scoring Table)",
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    }
+    // public static PathPlannerTrajectory LeaveCommunityPathScoringTable() {
+    // return PathPlanner.loadPath("Leave Community Path (Scoring Table)",
+    // AutoConstants.kMaxSpeedMetersPerSecond,
+    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    // }
 
-    public static PathPlannerTrajectory LeaveCommunityPathSubstation() {
-        return PathPlanner.loadPath("Leave Community Path (Substation)",
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    }
+    // public static PathPlannerTrajectory LeaveCommunityPathSubstation() {
+    // return PathPlanner.loadPath("Leave Community Path (Substation)",
+    // AutoConstants.kMaxSpeedMetersPerSecond,
+    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    // }
 
     public static PathPlannerTrajectory MountChargeStationPath() {
         return PathPlanner.loadPath("Mount Charge Station Path",
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+                AutoConstants.kMaxSpeedMetersPerSecond / 2,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared / 2);
     }
 
-    public static PathPlannerTrajectory TwoMeterPath() {
-        return PathPlanner.loadPath("Two Meter Path",
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    }
+    // public static PathPlannerTrajectory TwoMeterPath() {
+    // return PathPlanner.loadPath("Two Meter Path",
+    // AutoConstants.kMaxSpeedMetersPerSecond,
+    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    // }
 }
--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -3,8 +3,8 @@ package frc.robot.constants;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 public final class AutoConstants {
-    public static final double kMaxSpeedMetersPerSecond = 1.5;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 1.5;
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
     public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
     public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
 
