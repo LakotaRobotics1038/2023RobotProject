aeec709f6b7a0a777e17303fb2add2ad8d50e906
Nathan Burden <nburden355@gmail.com>
2023-03-02 15:37:07 -0500

    Reversed commenting

--- src/main/java/frc/robot/autons/Trajectories.java
+++ src/main/java/frc/robot/autons/Trajectories.java
@@ -6,23 +6,23 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import frc.robot.constants.AutoConstants;
 
 public class Trajectories {
-    // public static PathPlannerTrajectory LeaveCommunityPathCenter() {
-    // return PathPlanner.loadPath("Leave Community Path (Center)",
-    // AutoConstants.kMaxSpeedMetersPerSecond,
-    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    // }
+    public static PathPlannerTrajectory LeaveCommunityPathCenter() {
+        return PathPlanner.loadPath("Leave Community Path (Center)",
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    }
 
-    // public static PathPlannerTrajectory LeaveCommunityPathScoringTable() {
-    // return PathPlanner.loadPath("Leave Community Path (Scoring Table)",
-    // AutoConstants.kMaxSpeedMetersPerSecond,
-    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    // }
+    public static PathPlannerTrajectory LeaveCommunityPathScoringTable() {
+        return PathPlanner.loadPath("Leave Community Path (Scoring Table)",
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    }
 
-    // public static PathPlannerTrajectory LeaveCommunityPathSubstation() {
-    // return PathPlanner.loadPath("Leave Community Path (Substation)",
-    // AutoConstants.kMaxSpeedMetersPerSecond,
-    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    // }
+    public static PathPlannerTrajectory LeaveCommunityPathSubstation() {
+        return PathPlanner.loadPath("Leave Community Path (Substation)",
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    }
 
     public static PathPlannerTrajectory MountChargeStationPath() {
         return PathPlanner.loadPath("Mount Charge Station Path",
@@ -30,9 +30,9 @@ public class Trajectories {
                 AutoConstants.kMaxAccelerationMetersPerSecondSquared / 2);
     }
 
-    // public static PathPlannerTrajectory TwoMeterPath() {
-    // return PathPlanner.loadPath("Two Meter Path",
-    // AutoConstants.kMaxSpeedMetersPerSecond,
-    // AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-    // }
+    public static PathPlannerTrajectory TwoMeterPath() {
+        return PathPlanner.loadPath("Two Meter Path",
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    }
 }
