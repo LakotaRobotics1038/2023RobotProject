5ddc701f5439c06eddb4d16fc9854883937902cd
Wesley Reed <reed.wesley.s@gmail.com>
2023-03-07 18:26:58 -0500

    .

--- src/main/java/frc/robot/autons/LeaveCommunityPathCenterAuto.java
+++ src/main/java/frc/robot/autons/LeaveCommunityPathCenterAuto.java
@@ -7,10 +7,13 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 public class LeaveCommunityPathCenterAuto extends Auton {
     public LeaveCommunityPathCenterAuto(Alliance alliance) {
         super(alliance);
+
         PathPlannerTrajectory trajectory = alliance == Alliance.Blue ? Trajectories.LeaveCommunityPathCenterBlue()
                 : Trajectories.LeaveCommunityPathCenterRed();
+
         super.addCommands(
                 this.driveTrain.getTrajectoryCommand(trajectory));
+
         this.setInitialPose(trajectory);
     }
 }
