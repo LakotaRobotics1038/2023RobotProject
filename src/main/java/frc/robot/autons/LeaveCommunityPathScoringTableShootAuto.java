250919e01de81f06bcfc40f75290c2de6cc92dc0
Wesley Reed <reed.wesley.s@gmail.com>
2023-03-09 17:42:45 -0500

    shoot then leave community

---
+++ src/main/java/frc/robot/autons/LeaveCommunityPathScoringTableShootAuto.java
@@ -0,0 +1,20 @@
+package frc.robot.autons;
+
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.commands.ShootCubeCommand;
+import frc.robot.subsystems.CubeShooter.CubeShooterSetpoints;
+
+public class LeaveCommunityPathScoringTableShootAuto extends Auton {
+    public LeaveCommunityPathScoringTableShootAuto(Alliance alliance) {
+        super(alliance);
+        PathPlannerTrajectory trajectory = alliance == Alliance.Blue ? Trajectories.LeaveCommunityPathScoringTableBlue()
+                : Trajectories.LeaveCommunityPathScoringTableRed();
+
+        super.addCommands(
+                new ShootCubeCommand(CubeShooterSetpoints.high, 1.0),
+                this.driveTrain.getTrajectoryCommand(trajectory));
+        this.setInitialPose(trajectory);
+    }
+}
--- src/main/java/frc/robot/commands/ShootCubeCommand.java
+++ src/main/java/frc/robot/commands/ShootCubeCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.CubeShooter;
 import frc.robot.subsystems.CubeAcquisition.AcquisitionStates;
@@ -10,11 +11,19 @@ public class ShootCubeCommand extends CommandBase {
     private CubeShooter cubeShooter = CubeShooter.getInstance();
     private CubeAcquisition cubeAcquisition = CubeAcquisition.getInstance();
     private CubeShooterSetpoints setpoint;
+    private double secondsToShoot = 0.0;
     private boolean overrideFeedOut = false;
+    private double startTime = 0.0;
 
     public ShootCubeCommand(CubeShooterSetpoints setpoint) {
         this.addRequirements(cubeShooter, cubeAcquisition);
         this.setpoint = setpoint;
+    }
+
+    public ShootCubeCommand(CubeShooterSetpoints setpoint, double secondsToShoot) {
+        this.addRequirements(cubeShooter, cubeAcquisition);
+        this.setpoint = setpoint;
+        this.secondsToShoot = secondsToShoot;
     }
 
     @Override
@@ -27,6 +36,9 @@ public class ShootCubeCommand extends CommandBase {
     @Override
     public void execute() {
         if (overrideFeedOut || cubeShooter.onTarget()) {
+            if (this.startTime == 0.0 && this.secondsToShoot != 0) {
+                this.startTime = Timer.getFPGATimestamp();
+            }
             cubeShooter.feedOut();
         }
     }
@@ -37,6 +49,10 @@ public class ShootCubeCommand extends CommandBase {
 
     @Override
     public boolean isFinished() {
+        if (this.startTime != 0.0 && this.secondsToShoot != 0) {
+            return this.startTime + this.secondsToShoot < Timer.getFPGATimestamp();
+        }
+
         return false;
     }
 
