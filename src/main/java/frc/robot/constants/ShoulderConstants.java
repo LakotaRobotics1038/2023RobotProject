c7003f16b4894e7798839671c4307bb01ff4aaf4
Wesley Reed <reed.wesley.s@gmail.com>
2023-03-10 19:16:43 -0500

    update shoulder PID

--- src/main/java/frc/robot/constants/ShoulderConstants.java
+++ src/main/java/frc/robot/constants/ShoulderConstants.java
@@ -2,11 +2,11 @@ package frc.robot.constants;
 
 public class ShoulderConstants {
     public static final int kShoulderMotorPort = 10;
-    public static final double kMaxPower = 0.5;
+    public static final double kMaxPower = 0.75;
     public static final double kEncoderConversion = 360.0;
     public static final double kMaxDistance = 90.0;
     public static final double kTolerance = 2.0;
-    public static final double kP = 0.020;
+    public static final double kP = 0.040;
     public static final double kI = 0.000;
     public static final double kD = 0.000;
 }
