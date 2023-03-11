e3d92af80f1287fec06a50183e0f88cef66e8370
Wesley Reed <reed.wesley.s@gmail.com>
2023-03-11 12:03:06 -0500

    added balance to mount charge station

--- src/main/java/frc/robot/autons/MountChargeStationAuto.java
+++ src/main/java/frc/robot/autons/MountChargeStationAuto.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.commands.BalanceRobotCommand;
 
 public class MountChargeStationAuto extends Auton {
     public MountChargeStationAuto(Alliance alliance) {
@@ -12,6 +13,7 @@ public class MountChargeStationAuto extends Auton {
                 : Trajectories.MountChargeStationPathRed();
         super.addCommands(
                 this.driveTrain.getTrajectoryCommand(trajectory),
+                new BalanceRobotCommand(),
                 new RunCommand(() -> driveTrain.setX(), driveTrain));
         this.setInitialPose(trajectory);
     }
