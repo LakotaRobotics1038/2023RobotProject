a272e9b16eba4bf8b266e389d33deb73922d5ebd
Nox Heidt <zoe.heidt@gmail.com>
2023-03-16 15:03:59 -0400

    always shoot a cube

--- src/main/java/frc/robot/autons/LeaveCommunityPathScoringTableAuto.java
+++ src/main/java/frc/robot/autons/LeaveCommunityPathScoringTableAuto.java
@@ -3,7 +3,12 @@ package frc.robot.autons;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.CubeAcquisitionPositionCommand;
+import frc.robot.commands.ShootCubeCommand;
 import frc.robot.subsystems.Dashboard;
+import frc.robot.subsystems.CubeAcquisition.AcquisitionStates;
+import frc.robot.subsystems.CubeShooter.CubeShooterSetpoints;
 
 public class LeaveCommunityPathScoringTableAuto extends Auton {
     public LeaveCommunityPathScoringTableAuto(Alliance alliance) {
@@ -13,7 +18,11 @@ public class LeaveCommunityPathScoringTableAuto extends Auton {
 
         Dashboard.getInstance().setTrajectory(trajectory);
 
-        super.addCommands(this.driveTrain.getTrajectoryCommand(trajectory));
+        super.addCommands(
+                new ShootCubeCommand(CubeShooterSetpoints.high, 1.0),
+                new ParallelCommandGroup(
+                        this.driveTrain.getTrajectoryCommand(trajectory),
+                        new CubeAcquisitionPositionCommand(AcquisitionStates.Up)));
 
         this.setInitialPose(trajectory);
     }
