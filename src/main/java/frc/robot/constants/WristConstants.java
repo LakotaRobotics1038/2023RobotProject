70123e465239f82d0ff966691f50999b67485c26
Wesley Reed <reed.wesley.s@gmail.com>
2023-03-28 20:02:16 -0400

    turning wrist

--- src/main/java/frc/robot/constants/WristConstants.java
+++ src/main/java/frc/robot/constants/WristConstants.java
@@ -3,15 +3,15 @@ package frc.robot.constants;
 public class WristConstants {
     public static final int kWristMotorPort = 11;
     public static final double kEncoderConversion = 360.0;
-    public static final double kMaxPower = 0.75;
-    public static final double kP = 0.0042;
-    public static final double kI = 0.0001;
+    public static final double kMaxPower = 1.0;
+    public static final double kP = 0.0043;
+    public static final double kI = 0.0002;
     public static final double kD = 0.00;
     public static final double kTolerance = 5.0;
     public static final double kMaxDistance = 140;
 
     public static final int kStorageSetpoint = 0;
-    public static final int kCarrySetpoint = 20;
+    public static final int kCarrySetpoint = 24;
     public static final int kAcquireSetpoint = 75;
     public static final int kMidSetpoint = 106;
     public static final int kHumanPlayerSetpoint = 105;
