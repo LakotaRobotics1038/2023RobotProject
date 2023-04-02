ff4edbcc167f325d68d291b045653408d7b9334d
Wesley Reed <reed.wesley.s@gmail.com>
2023-04-02 14:54:52 -0400

    decrease hold speed

--- src/main/java/frc/robot/constants/ConeAcquisitionConstants.java
+++ src/main/java/frc/robot/constants/ConeAcquisitionConstants.java
@@ -3,5 +3,5 @@ package frc.robot.constants;
 public final class ConeAcquisitionConstants {
     public static final int kAcquisitionMotorPort = 12;
     public static final double kAcquireSpeed = 0.75;
-    public static final double kHoldConeSpeed = 0.1;
+    public static final double kHoldConeSpeed = 0.05;
 }
