2103022b232aae3f70ae79a2bf54dc2bad68b3c6
Wesley Reed <reed.wesley.s@gmail.com>
2023-04-11 19:28:20 -0400

    go back to old rate limiting

--- src/main/java/frc/robot/DriverXboxController.java
+++ src/main/java/frc/robot/DriverXboxController.java
@@ -4,6 +4,7 @@ import frc.robot.constants.DriveConstants;
 import frc.robot.constants.IOConstants;
 import frc.robot.libraries.XboxController1038;
 import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -11,6 +12,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class DriverXboxController extends XboxController1038 {
     // Subsystem Dependencies
     private final DriveTrain driveTrain = DriveTrain.getInstance();
+
+    // Previous Status
+    private double prevX = 0;
+    private double prevY = 0;
+    private double prevZ = 0;
 
     // Singleton Setup
     private static DriverXboxController instance;
@@ -26,31 +32,70 @@ public class DriverXboxController extends XboxController1038 {
     private DriverXboxController() {
         super(IOConstants.kDriverControllerPort);
 
-        driveTrain.setDefaultCommand(new RunCommand(() -> {
-            double sideways = super.getLeftX();
-            double forward = super.getLeftY();
-            double rotate = super.getRightX();
+        SlewRateLimiter forwardFilter = new SlewRateLimiter(1.0);
+        SlewRateLimiter sidewaysFilter = new SlewRateLimiter(1.0);
+        SlewRateLimiter rotateFilter = new SlewRateLimiter(1.0);
 
-            driveTrain.drive(forward, -sideways, -rotate, true, true);
+        driveTrain.setDefaultCommand(new RunCommand(() -> {
+            double x = super.getLeftX();
+            double y = super.getLeftY();
+            double z = super.getRightX();
+
+            double forward = limitRate(y, prevY, forwardFilter);
+            double sideways = limitRate(x, prevX, sidewaysFilter);
+            double rotate = limitRate(z, prevZ, rotateFilter);
+
+            prevX = x;
+            prevY = y;
+            prevZ = z;
+
+            driveTrain.drive(forward, -sideways, -rotate, true);
         }, driveTrain));
 
         new Trigger(() -> getPOVPosition() == PovPositions.Up)
                 .whileTrue(new RunCommand(() -> driveTrain
-                        .drive(DriveConstants.kFineAdjustmentPercent, 0, 0, true, false)));
+                        .drive(DriveConstants.kFineAdjustmentPercent, 0, 0, true)));
         new Trigger(() -> getPOVPosition() == PovPositions.Down)
                 .whileTrue(new RunCommand(() -> driveTrain
-                        .drive(-DriveConstants.kFineAdjustmentPercent, 0, 0, true, false)));
+                        .drive(-DriveConstants.kFineAdjustmentPercent, 0, 0, true)));
         new Trigger(() -> getPOVPosition() == PovPositions.Left)
                 .whileTrue(new RunCommand(() -> driveTrain
-                        .drive(0, DriveConstants.kFineAdjustmentPercent, 0, true, false)));
+                        .drive(0, DriveConstants.kFineAdjustmentPercent, 0, true)));
         new Trigger(() -> getPOVPosition() == PovPositions.Right)
                 .whileTrue(new RunCommand(() -> driveTrain
-                        .drive(0, -DriveConstants.kFineAdjustmentPercent, 0, true, false)));
+                        .drive(0, -DriveConstants.kFineAdjustmentPercent, 0, true)));
 
         // Re-orient robot to the field
         super.startButton.whileTrue(new InstantCommand(driveTrain::zeroHeading, driveTrain));
 
         // Lock the wheels into an X formation
         super.xButton.whileTrue(new RunCommand(driveTrain::setX, driveTrain));
+    }
+
+    /**
+     *
+     * @param value   Current desired value
+     * @param prevVal Previously desired value
+     * @param filter  SlewRateLimiter instance for calculation
+     * @return desired value rate limited and adjusted for sign changes using
+     *         {@link #signChange Sign Change Function}
+     */
+    private double limitRate(double value, double prevVal, SlewRateLimiter filter) {
+        if (value == 0 || signChange(value, prevVal)) {
+            filter.reset(0);
+        }
+        return filter.calculate(value);
+    }
+
+    /**
+     * Determines if the two given values are opposite signs
+     * (one positive one negative)
+     *
+     * @param a first value to check sign
+     * @param b second value to check sign
+     * @return are the provided values different signs
+     */
+    private boolean signChange(double a, double b) {
+        return a > 0 && b < 0 || b > 0 && a < 0;
     }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -9,7 +9,6 @@ import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -17,7 +16,6 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -25,7 +23,6 @@ import frc.robot.constants.AutoConstants;
 import frc.robot.constants.DriveConstants;
 import frc.robot.libraries.MAXSwerveModule;
 import frc.robot.libraries.Pigeon1038;
-import frc.robot.libraries.SwerveUtils;
 
 public class DriveTrain extends SubsystemBase {
     // Create MAXSwerveModules
@@ -51,15 +48,6 @@ public class DriveTrain extends SubsystemBase {
 
     // The gyro sensor
     public final Pigeon1038 gyro = new Pigeon1038(DriveConstants.kGyroCanId);
-
-    // Slew rate filter variables for controlling lateral acceleration
-    private double currentRotation = 0.0;
-    private double currentTranslationDir = 0.0;
-    private double currentTranslationMag = 0.0;
-
-    private SlewRateLimiter magLimiter = new SlewRateLimiter(DriveConstants.kMagnitudeSlewRate);
-    private SlewRateLimiter rotLimiter = new SlewRateLimiter(DriveConstants.kRotationalSlewRate);
-    private double prevTime = WPIUtilJNI.now() * 1e-6;
 
     // Odometry class for tracking robot pose
     SwerveDriveOdometry odometry = new SwerveDriveOdometry(
@@ -134,70 +122,18 @@ public class DriveTrain extends SubsystemBase {
      * @param rot           Angular rate of the robot.
      * @param fieldRelative Whether the provided x and y speeds are relative to the
      *                      field.
-     * @param rateLimit     Whether to enable rate limiting for smoother control.
      */
-    public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative, boolean rateLimit) {
-
-        double xSpeedCommanded;
-        double ySpeedCommanded;
-
-        if (rateLimit) {
-            // Convert XY to polar for rate limiting
-            double inputTranslationDir = Math.atan2(ySpeed, xSpeed);
-            double inputTranslationMag = Math.sqrt(Math.pow(xSpeed, 2) + Math.pow(ySpeed, 2));
-
-            // Calculate the direction slew rate based on an estimate of the lateral
-            // acceleration
-            double directionSlewRate;
-            if (currentTranslationMag != 0.0) {
-                directionSlewRate = Math.abs(DriveConstants.kDirectionSlewRate / currentTranslationMag);
-            } else {
-                directionSlewRate = 500.0; // some high number that means the slew rate is effectively instantaneous
-            }
-
-            double currentTime = WPIUtilJNI.now() * 1e-6;
-            double elapsedTime = currentTime - prevTime;
-            double angleDif = SwerveUtils.AngleDifference(inputTranslationDir, currentTranslationDir);
-            if (angleDif < 0.45 * Math.PI) {
-                currentTranslationDir = SwerveUtils.StepTowardsCircular(currentTranslationDir, inputTranslationDir,
-                        directionSlewRate * elapsedTime);
-                currentTranslationMag = magLimiter.calculate(inputTranslationMag);
-            } else if (angleDif > 0.85 * Math.PI) {
-                if (currentTranslationMag > 1e-4) {
-                    // some small number to avoid floating-point errors with equality checking keep
-                    // currentTranslationDir unchanged
-                    currentTranslationMag = magLimiter.calculate(0.0);
-                } else {
-                    currentTranslationDir = SwerveUtils.WrapAngle(currentTranslationDir + Math.PI);
-                    currentTranslationMag = magLimiter.calculate(inputTranslationMag);
-                }
-            } else {
-                currentTranslationDir = SwerveUtils.StepTowardsCircular(currentTranslationDir, inputTranslationDir,
-                        directionSlewRate * elapsedTime);
-                currentTranslationMag = magLimiter.calculate(0.0);
-            }
-            prevTime = currentTime;
-
-            xSpeedCommanded = currentTranslationMag * Math.cos(currentTranslationDir);
-            ySpeedCommanded = currentTranslationMag * Math.sin(currentTranslationDir);
-            currentRotation = rotLimiter.calculate(rot);
-
-        } else {
-            xSpeedCommanded = xSpeed;
-            ySpeedCommanded = ySpeed;
-            currentRotation = rot;
-        }
-
-        // Convert the commanded speeds into the correct units for the drivetrain
-        double xSpeedDelivered = xSpeedCommanded * DriveConstants.kMaxSpeedMetersPerSecond;
-        double ySpeedDelivered = ySpeedCommanded * DriveConstants.kMaxSpeedMetersPerSecond;
-        double rotDelivered = currentRotation * DriveConstants.kMaxAngularSpeed;
+    public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+        // Adjust input based on max speed
+        xSpeed *= DriveConstants.kMaxSpeedMetersPerSecond;
+        ySpeed *= DriveConstants.kMaxSpeedMetersPerSecond;
+        rot *= DriveConstants.kMaxAngularSpeed;
 
         SwerveModuleState[] swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
                 fieldRelative
-                        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered,
+                        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot,
                                 Rotation2d.fromDegrees(gyro.getAngle()))
-                        : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
+                        : new ChassisSpeeds(xSpeed, ySpeed, rot));
         this.setModuleStates(swerveModuleStates);
     }
 
@@ -236,7 +172,7 @@ public class DriveTrain extends SubsystemBase {
                 this::setModuleStates,
                 true,
                 this)
-                .andThen(() -> drive(0, 0, 0, false, false));
+                .andThen(() -> drive(0, 0, 0, false));
     }
 
     public void setDrivingIdleMode(IdleMode mode) {
