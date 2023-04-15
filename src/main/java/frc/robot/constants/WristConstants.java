86ba087a1bbfca0395c8478e948a51b2585a4f29
Wesley Reed <reed.wesley.s@gmail.com>
2023-04-15 15:39:01 -0400

    re-tune wrist

--- src/main/java/frc/robot/constants/WristConstants.java
+++ src/main/java/frc/robot/constants/WristConstants.java
@@ -4,8 +4,8 @@ public class WristConstants {
     public static final int kWristMotorPort = 11;
     public static final double kEncoderConversion = 360.0;
     public static final double kMaxPower = 1.0;
-    public static final double kP = 0.0043;
-    public static final double kI = 0.0004;
+    public static final double kP = 0.0042;
+    public static final double kI = 0.0001;
     public static final double kD = 0.00;
     public static final double kTolerance = 5.0;
     public static final double kMaxDistance = 260;
